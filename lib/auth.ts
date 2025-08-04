b34ac05369b0179b8fc0cfd7b4cabb5ef80ded49
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-08-04 21:45:30 +0200

    JWT,SESSION added

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
@@ -45,7 +46,32 @@ import bcrypt from "bcryptjs";
           console.log("authorize error",error)
           throw new Error("Invalid email or password")
         }
-      }
+      },
+
     })
-  ]
+    
+  ],
+  callbacks:{
+    async jwt({token, user}){
+      if(user){
+        token.id = user.id
+      }
+      return token
+    },
+    async session({session,token}){
+      if(session.user){
+       session.user.id=token.id as string
+      }
+      return session
+    }
+  },
+  pages:{
+    signIn:"/login",
+    error:"/login" 
+  },
+  session:{
+    strategy:"jwt",
+    maxAge:30*24*60*60,
+  },
+  secret:process.env.NEXTAUTH_SECRET, 
  }
